86a9726cd37f76a854550da686639f353eb39333
kajela <titoyut56@gmail.com>
2023-02-15 05:39:28 -0500

    add the initial files

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE aircraft;
 \c aircraft;
 CREATE TABLE airports(id SERIAL PRIMARY KEY, name VARCHAR, code VARCHAR, city VARCHAR, distance VARCHAR);
+CREATE TABLE features(id SERIAL PRIMARY KEY, widthrunway VARCHAR, lengthrunway VARCHAR, strengthrunway VARCHAR, airportid INTEGER, createdat BIGINT);
 CREATE DATABASE aircraft_test WITH TEMPLATE aircraft;
---
+++ src/test/java/dao/Sql2oFeatureDaoTest.java
@@ -0,0 +1,10 @@
+package dao;
+
+import org.sql2o.Connection;
+
+public class Sql2oFeatureDaoTest {
+    private static Connection conn;
+
+    private Sql2oFeatureDao featureDao;
+    private Sql2oAirportDao airportDao;
+}
