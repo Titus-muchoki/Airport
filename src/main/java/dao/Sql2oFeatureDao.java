4e21a00ea970e324e4128ffc8b5f6da8f276a6f8
kajela <titoyut56@gmail.com>
2023-02-15 05:26:16 -0500

    add the initial files

--- src/main/java/dao/Sql2oFeatureDao.java
+++ src/main/java/dao/Sql2oFeatureDao.java
@@ -1,7 +1,10 @@
 package dao;
 
 import models.Feature;
+import models.Review;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -15,17 +18,35 @@ public class Sql2oFeatureDao implements FeatureDao {
 
     @Override
     public void add(Feature feature) {
+        String sql = "INSERT INTO features (widthrunway, lengthrunway, strengthrunway, airportid, createdat) VALUES (:widthRunWay, :lengthRunWay, :strengthRunWay, :airportId, :createdat)"; //if you change your model, be sure to update here as well!
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(feature)
+                    .executeUpdate()
+                    .getKey();
+            feature.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
 
     }
 
     @Override
     public List<Feature> getAll() {
-        return null;
+        try(Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM features")
+                    .executeAndFetch(Feature.class);
+        }
     }
+
 
     @Override
     public List<Feature> getAllFeaturesByAirport(int airportId) {
-        return null;
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM features WHERE airportId = :airportId")
+                    .addParameter("airportId", airportId)
+                    .executeAndFetch(Feature.class);
+        }
     }
 
     @Override
