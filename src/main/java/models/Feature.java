60013b6478f1e03a43e5aff0a5a89094e22f7fc0
kajela <titoyut56@gmail.com>
2023-02-15 05:03:27 -0500

    add the initial files

--- src/main/java/models/Feature.java
+++ src/main/java/models/Feature.java
@@ -1,4 +1,22 @@
 package models;
 
 public class Feature {
+    private String widthRunWay;
+    private String lengthRunWay;
+    private String strengthRunWay;
+
+    private int id;
+    private int airportId;
+
+    private long createdat;
+    private String formattedCreatedAt;
+
+    public Feature(String widthRunWay, String lengthRunWay, String strengthRunWay, int airportId) {
+        this.widthRunWay = widthRunWay;
+        this.lengthRunWay = lengthRunWay;
+        this.strengthRunWay = strengthRunWay;
+        this.airportId = airportId;
+        this.createdat = System.currentTimeMillis();
+        setFormattedCreatedAt(); //we'll make me in a minute
+    }
 }
