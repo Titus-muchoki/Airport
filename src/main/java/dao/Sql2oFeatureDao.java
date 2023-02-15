0a4f425114702c314605760ac163b50e96c09522
kajela <titoyut56@gmail.com>
2023-02-15 05:32:12 -0500

    add the initial files

--- src/main/java/dao/Sql2oFeatureDao.java
+++ src/main/java/dao/Sql2oFeatureDao.java
@@ -59,11 +59,24 @@ public class Sql2oFeatureDao implements FeatureDao {
 
     @Override
     public void deleteById(int id) {
+        String sql = "DELETE from features WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
 
     }
 
     @Override
     public void clearAll() {
-
+        String sql = "DELETE from features";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 }
