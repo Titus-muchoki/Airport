50d91f6996249942b8e6ebc4ac4991b5261d11e5
kajela <titoyut56@gmail.com>
2023-02-15 01:55:33 -0500

    add the initial files

---
+++ src/main/java/dao/Sql2oAirportDao.java
@@ -0,0 +1,37 @@
+package dao;
+
+import models.Airport;
+
+import java.util.List;
+
+public class Sql2oAirportDao implements AirportDao{
+    @Override
+    public void add(Airport airport) {
+
+    }
+
+    @Override
+    public List<Airport> getAll() {
+        return null;
+    }
+
+    @Override
+    public Airport findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String code, String city, String distance) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}
