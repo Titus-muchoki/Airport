c7790c0322383b6b6b9435aa35f6874761413eb6
kajela <titoyut56@gmail.com>
2023-02-15 05:04:29 -0500

    add the initial files

--- src/main/java/models/Feature.java
+++ src/main/java/models/Feature.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Feature {
     private String widthRunWay;
     private String lengthRunWay;
@@ -18,5 +20,18 @@ public class Feature {
         this.airportId = airportId;
         this.createdat = System.currentTimeMillis();
         setFormattedCreatedAt(); //we'll make me in a minute
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Feature)) return false;
+        Feature feature = (Feature) o;
+        return id == feature.id && airportId == feature.airportId && createdat == feature.createdat && Objects.equals(widthRunWay, feature.widthRunWay) && Objects.equals(lengthRunWay, feature.lengthRunWay) && Objects.equals(strengthRunWay, feature.strengthRunWay) && Objects.equals(formattedCreatedAt, feature.formattedCreatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(widthRunWay, lengthRunWay, strengthRunWay, id, airportId, createdat, formattedCreatedAt);
     }
 }
