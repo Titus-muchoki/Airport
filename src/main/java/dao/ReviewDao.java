a118305ab894ed0e22f19b473f3b2dff947ab5f5
kajela <titoyut56@gmail.com>
2023-02-23 04:21:12 -0500

    add the initial files

--- src/main/java/dao/ReviewDao.java
+++ src/main/java/dao/ReviewDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import models.Feature;
+import models.Review;
+
+import java.util.List;
+
 public interface ReviewDao {
+    //create
+    void add(Feature feature);
+
+    //read
+    List<Review> getAll();
+    List<Review> getAllReviewsByAirport(int airportId);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
