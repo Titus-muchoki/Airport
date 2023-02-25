c75a462099ac00b120cd6641851a759fcbe881ed
kajela <titoyut56@gmail.com>
2023-02-25 03:39:01 -0500

    add the initial files

--- src/main/java/dao/Sql2oActivityDao.java
+++ src/main/java/dao/Sql2oActivityDao.java
@@ -2,12 +2,19 @@ package dao;
 
 import models.Activity;
 import models.Review;
+import org.sql2o.Sql2o;
 
 import java.util.List;
 
 public class Sql2oActivityDao implements ActivityDao{
+    public Sql2o sql2o;
+    public Sql2oActivityDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
     @Override
     public void add(Activity activity) {
+        String sql = "INSERT INTO activities()VALUES()";
 
     }
 
@@ -15,7 +22,6 @@ public class Sql2oActivityDao implements ActivityDao{
     public List<Activity> getAll() {
         return null;
     }
-
     @Override
     public List<Review> getAllActivitiesByAirport(int airportId) {
         return null;
