727dcace48b69878ee57f051064d0def0dfb01ac
kajela <titoyut56@gmail.com>
2023-02-25 03:29:39 -0500

    add the initial fies

---
+++ src/main/java/dao/Sql2oActivityDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+import models.Activity;
+import models.Review;
+
+import java.util.List;
+
+public class Sql2oActivityDao implements ActivityDao{
+    @Override
+    public void add(Activity activity) {
+
+    }
+
+    @Override
+    public List<Activity> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Review> getAllActivitiesByAirport(int airportId) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}
